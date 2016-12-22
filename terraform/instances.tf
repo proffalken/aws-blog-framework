f0e5a7f6b2af53d2a2f5c716caaaf8ec890425ed
Matthew Macdonald-Wallace <theprofessor@threedrunkensysadsonthe.net>
2016-12-22 15:43:08 +0000

    Update tags/name to show in correct inventory group

--- terraform/instances.tf
+++ terraform/instances.tf
@@ -1,12 +1,12 @@
 resource "aws_instance" "web" {
   ami = "${lookup(var.centos_amis, var.region)}"
   instance_type = "t2.nano"
-  subnet_id = "${aws_subnet.public.id}"
+  subnet_id = "${aws_subnet.private.id}"
   vpc_security_group_ids = ["${aws_security_group.default.id}","${aws_security_group.web.id}"]
   key_name = "${aws_key_pair.aws-blog-deployer.key_name}"
   source_dest_check = false
   tags = {
-    Name = "web01"
+    Name = "web01-aws-blog"
     ansible_group_web_servers = "1"
   }
   connection {
